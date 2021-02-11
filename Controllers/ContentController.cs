3fd484af4b1578e7a6a6afd8291a10c278be8d44
adrian1230 <adrian1145d98@gmail.com>
2021-02-12 01:31:09 +0800

    adrian

---
+++ Controllers/ContentController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using bloopers.Models;
+
+namespace bloopers.Controllers
+{
+    public class ContentController : Controller
+    {
+        private readonly ILogger<ContentController> _logger;
+
+        public ContentController(ILogger<ContentController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
